dbda234bfd8a331ac3e20b80c8be04e2b4d58fb9
El-Sueco <svenstauder@outlook.at>
2024-03-25 17:29:59 +0100

    material toggle

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule
   ],
   providers: [
     provideAnimationsAsync()
